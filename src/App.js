31e036e2abede4443240b7eaf77cfdf2597c7b6d
Julia Reva <98896958+juliareva@users.noreply.github.com>
2024-02-21 20:56:33 -0800

    assets added, components created, meta tags added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Header from './components/Header';
+import Main from './components/Main';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      Home
+      <Navbar />
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import logo from "../icons_assets/Logo.svg"
+
+const Footer = () => {
+  return (
+    <footer>
+       <img src={logo} alt="logo"/>
+       <div>
+        <p>Navigation</p>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Menu</a></li>
+        </ul>
+        <ul>
+            <li><a href="#">Reservations</a></li>
+            <li><a href="#">Order Online</a></li>
+            <li><a href="#">Login</a></li>
+        </ul>
+       </div>
+       <div>
+        <p>Contact Us</p>
+        <ul>
+            <li><a href="#">Address</a></li>
+            <li><a href="#">Phone</a></li>
+            <li><a href="#">E-mail</a></li>
+        </ul>
+       </div>
+       <div>
+        <p>Social Media</p>
+        <ul>
+            <li><a href="#">Instagram</a></li>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Twitter</a></li>
+        </ul>
+       </div>
+    </footer>
+  )
+}
+
+export default Footer
